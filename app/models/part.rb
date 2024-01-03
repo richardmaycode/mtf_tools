d4d49292222e26c317c91601bb5ea56e62fbc59f
Richard Wise <richard.wise@hey.com>
2024-01-02 22:30:40 -0500

    add methods for calculating dates of each part in workout plan

--- app/models/part.rb
+++ app/models/part.rb
@@ -5,4 +5,17 @@ class Part < ApplicationRecord
   validates :week, presence: true, numericality: { in: 1..12 }
   validates :day, presence: true, numericality: { in: 1..5 }
   validates :segment, presence: true, numericality: { in: 1..3 }
+
+  def date_modifier
+    return 0 unless week > 1
+    return week * 7 unless day > 1
+
+    (week * 7) + day
+  end
+
+  def expected_date(start_date)
+    return start_date if week && day == 1
+
+    start_date + date_modifier.days
+  end
 end
